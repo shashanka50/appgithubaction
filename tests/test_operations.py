f01b2403d918b3413100c8869bc66e8bda449e33
shashanka50 <shashank.g2398@gmail.com>
2024-09-04 10:51:39 +0530

    modified testing command

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,5 @@
 # from src import operations
-from src.math_operations import add,sub
+from src.math_operations import add,sub,mul
 
 def test_add():
     assert add(2,3) == 5
@@ -14,3 +14,10 @@ def test_sub():
     assert sub(-2,-3) == 1
     assert sub(1,1) == 0
     assert sub(-2,3) == -5
+
+def test_mul():
+    assert mul(2,3) == 6
+    assert mul(2,-3) == -6
+    assert mul(-2,-3) == 6
+    assert mul(1,1) == 1
+    assert mul(-2,3) == -6
