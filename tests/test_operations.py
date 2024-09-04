aa2846662bb0d79cfc8863dbb9c25bff1e172fcc
shashanka50 <shashank.g2398@gmail.com>
2024-09-04 15:29:40 +0530

    Added div operations and tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -21,3 +21,10 @@ def test_mul():
     assert mul(-2,-3) == 6
     assert mul(1,1) == 1
     assert mul(-2,3) == -6
+
+def test_div():
+    assert div(2,3) == 2/3
+    assert div(2,-3) == 2/-3
+    assert div(-2,-3) == 2/3
+    assert div(1,1) == 1
+    assert div(-2,3) == -2/3
